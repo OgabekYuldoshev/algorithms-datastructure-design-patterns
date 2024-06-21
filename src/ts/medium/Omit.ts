13d8dcff808844527fe8580f9ad95a51640206d4
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-06-21 15:27:23 +0500

    feat: 🔥 create custom omit type

---
+++ src/ts/medium/Omit.ts
@@ -0,0 +1,12 @@
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type MyOmit<T, K extends keyof T> = {
+    [P in keyof T as P extends K ? never : P]: T[P] 
+};
+
+type TodoPreview = MyOmit<Todo, "description" | 'completed'>;
+//   ^?
